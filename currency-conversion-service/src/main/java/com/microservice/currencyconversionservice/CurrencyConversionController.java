22fe8d6dd94c3c226f56aa5dcc3d6edf33e2a4c3
sunilrao <g.sunilrao@gmail.com>
2023-02-13 00:44:09 +0530

    Second commit work in progress

--- currency-conversion-service/src/main/java/com/microservice/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/microservice/currencyconversionservice/CurrencyConversionController.java
@@ -26,7 +26,7 @@ public class CurrencyConversionController {
         uriVariables.put("from", from);
         uriVariables.put("to", to);
         ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
-                .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                .getForEntity("http://currency-exchange-service:8000/currency-exchange/from/{from}/to/{to}",
                         CurrencyConversion.class, uriVariables);
         CurrencyConversion currencyConversion = responseEntity.getBody();
         return new CurrencyConversion(currencyConversion.getId(),
